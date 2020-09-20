76206a282e6834d8b17a1701520eb500550e7b1d
Dimas Purnomo <55596701+Dimzdim@users.noreply.github.com>
2020-09-20 19:00:44 +0700

    Homepage

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PEMILIHAN UMUM HIMAKOM</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-left">
+          <img class="logo" src="images/main_logo.png">
+        </div>
+      </div>
+    </header>
+     <div class="top-wrapper">
+      <div class="container">
+        <img src='images/himakom.png' width='200' height='200' />
+        <h1>PEMILIHAN UMUM</h1>
+        <h1>KETUA HIMPUNAN MAHASISWA ILMU KOMPUTER 2020-2021</h1>
+        <div class="btn-wrapper">
+ 
+        </div>
+      </div>
+    </div>
+    <div class="lesson-wrapper">
+      <div class="container">
+        <div class="heading">
+          <h2>Cara Memilih Ketua Himpunan</h2>
+        </div>
+        <div class="lessons">
+          <div class="lesson">
+            <div class="lesson-icon">
+              <img src="images/1.png" height="150" width="150">
+            <p class="text-contents">Panitia KPU akan Memberikan Akun kepada Mahasiswa untuk memilih Ketua Himpunan</p>
+            </div>
+          </div>
+          <div class="lesson">
+            <div class="lesson-icon">
+              <img src="images/2.png" height="150" width="150">
+            </div>
+            <p class="text-contents">Mahasiswa Login dengan Akun yang sudah diberikan panitia</p>
+       </div>
+          <div class="lesson">
+            <div class="lesson-icon">
+            <img src="images/3.png" height="150" width="150">
+            </div>
+            <p class="text-contents">Mahasiswa akan dihadapkan dengan Foto beserta Visi Misi Calon Ketua Himpunan.</p>
+          </div>
+          <div class="lesson">
+            <div class="lesson-icon">
+              <img src="images/4.png"  height="150" width="150">
+            </div>
+            <p class="text-contents">Untuk memilih kandidat dengan menekan Foto Calon Ketua Himpunan, Selesai</p>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
+    </div>
+    <div class="message-wrapper">
+      <div class="container">
+        <div class="heading">
+          <h2>Sukseskan Pemilihan Ketua Himpunan Mahasiswa Ilmu Komputer - Univesitas Pakuan!</h2>
+          <h3>Untuk Ilmu Komputer yang lebih baik</h3>
+        </div>
+        <a href="#" class="btn signup">Login</a>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <img src="#">
+        <p>Learn to code, learn to be creative.</p>
+      </div>
+    </footer>
+  </body>
+</html>
